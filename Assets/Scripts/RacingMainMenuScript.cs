4a6c3a34953662560e6b5a2dc813d4ff501fef0d
drantino <65998488+drantino@users.noreply.github.com>
2026-01-23 11:59:51 -0700

    basic menu setup

--- Assets/Scripts/RacingMainMenuScript.cs
+++ Assets/Scripts/RacingMainMenuScript.cs
@@ -1,11 +1,18 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RacingMainMenuScript : MonoBehaviour
 {
+    GameState gameState;
+
+    public GameObject sliderR, sliderG, sliderB;
+    public TextMeshProUGUI txtR, txtG, txtB;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameState = GameObject.FindGameObjectWithTag("GameState").GetComponent<GameState>();
     }
 
     public void StartGame()
@@ -15,5 +22,37 @@ public class RacingMainMenuScript : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+    public void VehicleTypeSelect(int typeID)
+    {
+        gameState.currentProfile.vehicleType = typeID;
+        //update preview
+    }
+    public void RGBSelectUpdate(int RGBID)
+    {
+        //RGBID 0 = Red, 1 = Green, 2 = Blue
+        switch (RGBID)
+        {
+            case 0:
+                { 
+                    txtR.text = "R:"+sliderR.GetComponent<Slider>().value.ToString();
+                    gameState.currentProfile.vehicleColorR = sliderR.GetComponent<Slider>().value;
+                    break;
+                }
+            case 1:
+                {
+                    break;
+                }
+            case 2:
+                {
+
+                    break;
+                }
+            default:
+                {
+                    Debug.Log("Invalid RGBID");
+                    break;
+                }
+        }
     }
 }
