162a5256eb0895c71893711ec035818e3c19da21
drantino <65998488+drantino@users.noreply.github.com>
2026-01-25 17:33:55 -0700

    time laps tracker

---
+++ Assets/Scripts/RacingUIScript.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class RacingUIScript : MonoBehaviour
+{
+    public GameState gameState;
+    public float timeLaps;
+    public TMP_Text timeTxtValue;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameState = GameObject.FindGameObjectWithTag("GameState").GetComponent<GameState>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(gameState.currentGameState == GameState.RacingGameState.Racing)
+        {
+            timeLaps += Time.fixedDeltaTime;
+            timeTxtValue.text = (timeLaps / 60 - (timeLaps / 60 % 1)).ToString() + ":" + (timeLaps % 60 - (timeLaps % 60 % 0.01)).ToString();
+        }
+    }
+}
