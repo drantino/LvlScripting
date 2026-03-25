b5f0ab70e1950d24d9282b59bfc417fdd004bebd
drantino <65998488+drantino@users.noreply.github.com>
2026-03-25 13:42:59 -0600

    Delayed Scene trigger and removal

--- Assets/Scripts/SceneManagerAssignemnt/SceneTrigger.cs
+++ Assets/Scripts/SceneManagerAssignemnt/SceneTrigger.cs
@@ -1,22 +1,41 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTrigger : MonoBehaviour
 {
     Rigidbody2D myRigidBody;
     public string sceneName;
+    bool triggeredLoad;
+    public GameObject loadUI;
+    public Slider progressBar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        triggeredLoad = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Trigger");
-        if (collision != null && collision.CompareTag("Player"))
+        if (!triggeredLoad && collision != null && collision.CompareTag("Player"))
         {
-            SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            triggeredLoad=true;
+            StartCoroutine(AdditiveSceneLoad(sceneName));
+            loadUI.SetActive(true);
         }
+    }
+    public IEnumerator AdditiveSceneLoad(string sceneNameToLoad)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneNameToLoad, LoadSceneMode.Additive);
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            progressBar.value = progress;
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+        loadUI.SetActive(false);
+        this.gameObject.SetActive(false);
     }
 }
