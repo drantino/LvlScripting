fae0e068d8cdfe5d2669a841ebda784f2c81258f
drantino <65998488+drantino@users.noreply.github.com>
2026-03-31 14:02:03 -0600

    inventories show armor/weapon rating

--- Assets/Scripts/Inventory/ContainerBuutton.cs
+++ Assets/Scripts/Inventory/ContainerBuutton.cs
@@ -8,6 +8,7 @@ public class ContainerBuutton : MonoBehaviour
     public TMP_Text flavourText;
     public Image icon;
     public TMP_Text quanityDisplay;
+    public TMP_Text statRatingTxt;
     public InventoryItemData itemData;
     private InventoryContainer inventoryContainer;
     private bool isContainerButton;
@@ -22,8 +23,21 @@ public class ContainerBuutton : MonoBehaviour
         flavourText.text = itemData.flavourText;
         quanityDisplay.text = itemData.quantity.ToString();
         icon.sprite = itemData.icon;
+        //add rating if right type
+        if(itemData.itemType == ItemType.WEAPON)
+        {
+            statRatingTxt.text = ((itemData as WeaponItemData).weaponStrength + ":Weapon Str").ToString();
+        }
+        else if(itemData.itemType == ItemType.HELM || itemData.itemType == ItemType.ARMOR || itemData.itemType == ItemType.SHIELD)
+        {
+             statRatingTxt.text = ((itemData as ArmorItemData).armorRating + ":Armor Def").ToString();
+        }
+        else
+        {
+            statRatingTxt.text = null;
+        }
 
-        GetComponent<Button>().onClick.AddListener(ButtonClick);
+            GetComponent<Button>().onClick.AddListener(ButtonClick);
     }
     public void ButtonClick()
     {
--- Assets/Scripts/Inventory/InventoryButtonUI.cs
+++ Assets/Scripts/Inventory/InventoryButtonUI.cs
@@ -9,6 +9,7 @@ public class InventoryButtonUI : MonoBehaviour
     public Image icon;
     public TMP_Text quanityDisplay;
     public InventoryItemData itemData;
+    public TMP_Text statRatingTxt;
 
     public void InitalizeButton(InventoryItemData inventoryItemData)
     {
@@ -17,6 +18,18 @@ public class InventoryButtonUI : MonoBehaviour
         quanityDisplay.text = inventoryItemData.quantity.ToString();
         icon.sprite = inventoryItemData.icon;
         itemData = inventoryItemData;
+        if (itemData.itemType == ItemType.WEAPON)
+        {
+            statRatingTxt.text = ((itemData as WeaponItemData).weaponStrength + ":Weapon Str").ToString();
+        }
+        else if (itemData.itemType == ItemType.HELM || itemData.itemType == ItemType.ARMOR || itemData.itemType == ItemType.SHIELD)
+        {
+            statRatingTxt.text = ((itemData as ArmorItemData).armorRating + ":Armor Def").ToString();
+        }
+        else
+        {
+            statRatingTxt.text = null;
+        }
         GetComponent<Button>().onClick.AddListener(ButtonClick);
     }
     public void ButtonClick()
