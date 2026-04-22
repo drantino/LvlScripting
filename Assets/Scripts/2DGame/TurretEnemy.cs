39932a6d277d9d520e7e1529903e76cd39e7a726
drantino <65998488+drantino@users.noreply.github.com>
2026-04-22 14:45:44 -0600

    testing different stuff

--- Assets/Scripts/2DGame/TurretEnemy.cs
+++ Assets/Scripts/2DGame/TurretEnemy.cs
@@ -1,8 +1,10 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class TurretEnemy : Enemy
 {
     public GameObject attackPivot;
+    public float aimSpeed;
 
     public override void Attack()
     {
@@ -14,8 +16,19 @@ public class TurretEnemy : Enemy
         base.Update();
         if(attackRange.CircleOverLapCheck())
         {
-            attackPivot.transform.LookAt(new Vector3( playerPosition.x,playerPosition.y) - transform.position);
-            Debug.Log( new Vector3(playerPosition.x, playerPosition.y) - transform.position);
+            Pose pose = new();
+            //pose.position = attackPivot.transform.position;
+            
+            pose.rotation = Quaternion.LookRotation(new Vector3( playerPosition.x,playerPosition.y,0)- attackPivot.transform.position,attackPivot.transform.up);
+            float angle = Mathf.Atan((playerPosition.y - attackPivot.transform.position.y)/ (playerPosition.x - attackPivot.transform.position.x));
+            Debug.Log(angle);
+            pose.rotation.x = angle;
+            pose.rotation = quaternion.Euler(pose.rotation.eulerAngles.x,90,0);
+            //attackPivot.transform.
+            
+
+            attackPivot.transform.rotation = Quaternion.Slerp(attackPivot.transform.rotation,pose.rotation,aimSpeed);
+            
         }
     }
    
